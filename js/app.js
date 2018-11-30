c792dfee3580bf9e121b3612cd20a2efca39a2c0
Julie Singh <julie@juliesingh.tech>
2018-11-30 13:49:58 -0800

    add names and counter functionality

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
-const cat = document.querySelector('.cat');
-let clicks = document.querySelector('.clicks')
 
-cat.addEventListener('click', function() {
-  clicks.innerHTML++
+const name = ["Maddie", "Boots"]
+const catNames = document.querySelectorAll('.name')
+const cats = document.querySelectorAll('.cat');
+let clicks = document.querySelectorAll('.clicks')
+
+ for(let i=0; i<catNames.length; i++){
+   catNames[i].innerHTML = name[i]
+ }
+
+for(let i=0; i<cats.length; i++){
+  cats[i].addEventListener('click', function() {
+    clicks[i].innerHTML++
 })
+
+}
